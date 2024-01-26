58a13713ab0b1e62adc350a0d7edebf49d2c105b
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-01-26 18:50:07 +0330

    feat: add the django debug toolbar package middleware

--- johns_studio/settings.py
+++ johns_studio/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    # Installed Apps
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
